a55fab425472b54505bd78899fdad9e8a405d33e
gokmenozkn <kanjey159@gmail.com>
2020-06-22 14:08:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      duty: '',
+      tasks: []
+    }
+  }
+
+  addTask = (e) => {
+    e.preventDefault();
+    
+    if (this.state.duty) {
+      let todos = { text: this.state.duty, id: Date.now() };
+
+      this.state.tasks.push(todos);
+
+      this.setState({ tasks: this.state.tasks });
+
+    }
+    this.setState({ duty: '' });
+  }
+
+  changeDuty = e => {
+    this.setState({ duty: e.target.value });
+  }
+
+  removeTodo = id => {
+    const filtered = this.state.tasks.filter(task => {
+      return task.id !== id;
+    });
+    this.setState({ tasks: filtered });
+  }
+  
+  render() {
+    return (
+      <div className="main">
+        <div className="form">
+          <form onSubmit={this.addTask}>
+            <input 
+              type="input" 
+              className="form__field" placeholder="Enter Task"
+              value={this.state.duty}
+              onChange={this.changeDuty}
+              />
+          </form>
+        </div>
+        <div className="todo">
+          {this.state.tasks.length ?
+            '' : <p></p>
+          }
+          {this.state.tasks.map(task => (
+            <p key={task.id}>
+              {task.text}
+              <i 
+                className="material-icons icon" 
+                key={task.id}
+                onClick={() => this.removeTodo(task.id)}
+                >
+                delete
+              </i>
+            </p>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
